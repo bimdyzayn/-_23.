db7d7ebde921e290d9ba2a207a06e8779df028ac
dagremont@mail.ru <dagremont@mail.ru>
2021-12-19 13:27:47 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace задание_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            int n = int.Parse(Console.ReadLine());
+
+             Fact(n);
+
+
+            Console.ReadLine();
+        }
+        private static async Task Fact(int n)
+        {
+
+
+         
+            int factorial = 1;   
+
+            for (int i = 2; i <= n; i++) 
+            {
+                factorial = factorial * i;
+            }
+
+            Console.WriteLine($"Факториал числа  = {factorial}");
+            await Task.Delay(1000);
+        }
+   
+
+    }
+}
